2d11bb7ea8dc00db0c77ca8318969f3908b75dda
Abdelkhalek Amer <AbdelkhalekAmer@outlook.com>
2019-12-27 17:04:17 +0200

    add threading pool demo

---
+++ ThreadPoolDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    internal class Program
+    {
+        private static int count = 0;
+        private static object lockObject = new object();
+        private static void Main(string[] args)
+        {
+            
+            Thread.CurrentThread.Name = "Main thread";
+            var employee = new Employee()
+            {
+                Name = "Abdelkhalek Amer",
+                CompanyName = "Integrant Inc."
+            };
+
+            new Thread(() =>
+            {
+                printEmployeeInfo(employee);
+            }).Start();
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback(printEmployeeInfo), employee);
+
+            printEmployeeInfo(employee);
+
+        }
+        private static void printEmployeeInfo(object input)
+        {
+            if (Thread.CurrentThread.Name != "Main thread")
+            {
+                var isThreadPoolThreadMessage = Thread.CurrentThread.IsThreadPoolThread ? "(in thread pool)" :
+                    "(not in thread pool)";
+                lock (lockObject)
+                {
+                    Thread.CurrentThread.Name = $"Worker{++count} thread {isThreadPoolThreadMessage}";
+                }
+            }
+            var employee = input as Employee;
+            if (employee != null)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: {employee.Name} is working in {employee.CompanyName}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid employee data.");
+            }
+        }
+    }
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
