ce7dfbc7dc75ca2236f7a22479cb8bb9bc2064c2
Abdelkhalek Amer <Abdelkhalek.Amer@integrant.com>
2020-01-01 18:46:59 +0200

    Mutex locking

---
+++ MutexLocking/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace MutexLocking
+{
+    class Program
+    {
+        static Mutex mutex = new Mutex();
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main thread";
+            new Thread(AcquuireMutex) { Name = "worker thread 1" }.Start();
+            new Thread(AcquuireMutex) { Name = "worker thread 2" }.Start();
+            new Thread(AcquuireMutex) { Name = "worker thread 3" }.Start();
+
+        }
+
+        static void AcquuireMutex()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}: trying to acquire mutex");
+            mutex.WaitOne();
+            Console.WriteLine($"{Thread.CurrentThread.Name}: protected code by mutex");
+            mutex.ReleaseMutex();
+            Console.WriteLine($"{Thread.CurrentThread.Name}: mutex is released");
+        }
+
+    }
+}
