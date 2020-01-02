531ad43271290437ac11876f10a0e67393e2775e
Abdelkhalek Amer <Abdelkhalek.Amer@integrant.com>
2020-01-02 15:19:07 +0200

    Mutex

--- MutexLocking/Program.cs
+++ MutexLocking/Program.cs
@@ -9,19 +9,42 @@ namespace MutexLocking
         static void Main(string[] args)
         {
             Thread.CurrentThread.Name = "Main thread";
-            new Thread(AcquuireMutex) { Name = "worker thread 1" }.Start();
-            new Thread(AcquuireMutex) { Name = "worker thread 2" }.Start();
-            new Thread(AcquuireMutex) { Name = "worker thread 3" }.Start();
+            new Thread(AcquireMutex_TimedMutexAcuisition) { Name = "worker thread 1" }.Start();
+            new Thread(AcquireMutex_TimedMutexAcuisition) { Name = "worker thread 2" }.Start();
+            new Thread(AcquireMutex_TimedMutexAcuisition) { Name = "worker thread 3" }.Start();
 
+        }
+
+        static void AcquireMutex_TimedMutexAcuisition()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}: trying to acquire mutex");
+            if (mutex.WaitOne(1000, false))
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: protected code by mutex");
+                Thread.Sleep(2000);
+                mutex.ReleaseMutex();
+                Console.WriteLine($"{Thread.CurrentThread.Name}: mutex is released");
+            }
+            else
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: couldn't acquire mutex");
+            }
         }
 
         static void AcquuireMutex()
         {
             Console.WriteLine($"{Thread.CurrentThread.Name}: trying to acquire mutex");
-            mutex.WaitOne();
-            Console.WriteLine($"{Thread.CurrentThread.Name}: protected code by mutex");
-            mutex.ReleaseMutex();
-            Console.WriteLine($"{Thread.CurrentThread.Name}: mutex is released");
+            if (mutex.WaitOne())
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: protected code by mutex");
+                mutex.ReleaseMutex();
+                Console.WriteLine($"{Thread.CurrentThread.Name}: mutex is released");
+            }
+            else
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: couldn't acquire mutex");
+            }
+
         }
 
     }
