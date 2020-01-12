7eb45c83fe973ee74a90826f8d180881f5accad9
Abdelkhalek Amer <Abdelkhalek.Amer@integrant.com>
2020-01-12 12:56:44 +0200

    attach task to a parent task

---
+++ AttachedAndDetachedTasks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AttachedAndDetachedTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine($"{Task.CurrentId}: started");
+                Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine($"{Task.CurrentId}: is calling...");
+                }, TaskCreationOptions.AttachedToParent);
+                Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine($"{Task.CurrentId}: is calling...");
+                }, TaskCreationOptions.AttachedToParent);
+                Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine($"{Task.CurrentId}: is calling...");
+                }, TaskCreationOptions.AttachedToParent);
+                Console.WriteLine($"{Task.CurrentId}: completed");
+            });
+            task.Wait();
+            Console.WriteLine("All finished.");
+
+        }
+    }
+}
