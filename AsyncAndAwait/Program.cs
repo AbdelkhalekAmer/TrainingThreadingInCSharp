b064c5180962136c118d7a82d2454e7a42777b12
Abdelkhalek Amer <Abdelkhalek.Amer@integrant.com>
2020-01-16 13:26:48 +0200

    some async and await checking

---
+++ AsyncAndAwait/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Hello World!");
+            Display().Wait();
+            Console.ReadKey();
+        }
+
+        static async Task Display()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: {await GetInnerMessage()}");
+        }
+
+        static async Task<string> GetInnerMessage()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: getting inner message");
+
+            return await Task.Run(() =>
+            {
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: getting inner message data");
+                return "Inner Message Data";
+            });
+
+            //return task.Result;
+        }
+
+    }
+}
